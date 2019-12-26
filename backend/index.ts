2588f3b4fb936dd8e36c11422c448160ddc75ced
Alpha Chen <alpha@kejadlen.dev>
2019-12-26 14:11:11 -0800

    PORT

--- backend/index.ts
+++ backend/index.ts
@@ -4,6 +4,7 @@ import process from "process";
 import express from "express";
 
 const frontendPath = process.env.FRONTEND_PATH!;
+const port = process.env.PORT || 3001;
 
 const app = express();
 
@@ -13,4 +14,4 @@ app.get("/", function(_req, res) {
   res.sendFile(path.join(frontendPath, "index.html"));
 });
 
-app.listen(3001);
+app.listen(port);
