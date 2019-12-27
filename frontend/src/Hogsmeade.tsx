d81b39efca4dbc56b1479dc34c083bd47a8dc57c
Alpha Chen <alpha@kejadlen.dev>
2019-12-26 17:03:52 -0800

    add default graph

--- frontend/src/Hogsmeade.tsx
+++ frontend/src/Hogsmeade.tsx
@@ -2,7 +2,35 @@ import React, { useEffect, useState } from "react";
 import styles from "./Hogsmeade.module.css";
 
 const Hogsmeade: React.FC = () => {
-  const [dot, setDot] = useState("");
+  const [dot, setDot] = useState(`
+    digraph G {
+
+      subgraph cluster_0 {
+        style=filled;
+        color=lightgrey;
+        node [style=filled,color=white];
+        a0 -> a1 -> a2 -> a3;
+        label = "process #1";
+      }
+
+      subgraph cluster_1 {
+        node [style=filled];
+        b0 -> b1 -> b2 -> b3;
+        label = "process #2";
+        color=blue
+      }
+      start -> a0;
+      start -> b0;
+      a1 -> b3;
+      b2 -> a3;
+      a3 -> a0;
+      a3 -> end;
+      b3 -> end;
+
+      start [shape=Mdiamond];
+      end [shape=Msquare];
+    }
+  `);
   const [graph, setGraph] = useState<Buffer | null>(null);
 
   useEffect(() => {
